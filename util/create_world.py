03b4ae142886ae8c08346beefead0896a4e42a5b
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-01-18 14:56:15 -0600

    update room model method to connect rooms

--- util/create_world.py
+++ util/create_world.py
@@ -5,21 +5,15 @@ from adventure.models import Player, Room
 Room.objects.all().delete()
 
 r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
+               description="You're on Outside Cave Entrance.")
 
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east.""")
+r_foyer = Room(title="Foyer", description="You're on Foyer.")
 
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""")
+r_overlook = Room(title="Grand Overlook", description="You're on Grand Overlook.")
 
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air.""")
+r_narrow = Room(title="Narrow Passage", description="You're on Narrow Passage")
 
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""")
+r_treasure = Room(title="Treasure Chamber", description="You're on Treasure Chambe.r")
 
 r_outside.save()
 r_foyer.save()
@@ -29,19 +23,14 @@ r_treasure.save()
 
 # Link rooms together
 r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
 
 r_foyer.connectRooms(r_overlook, "n")
-r_overlook.connectRooms(r_foyer, "s")
 
 r_foyer.connectRooms(r_narrow, "e")
-r_narrow.connectRooms(r_foyer, "w")
 
 r_narrow.connectRooms(r_treasure, "n")
-r_treasure.connectRooms(r_narrow, "s")
 
 players=Player.objects.all()
 for p in players:
   p.currentRoom=r_outside.id
   p.save()
-
